14decc3929d80a89911e16299de8c0172349b39f
tcfplayz <58693033+ckclol@users.noreply.github.com>
2022-03-22 00:35:42 +0800

    Create InventoryAPI.java

---
+++ src/ml/spmc/invapi/InventoryAPI.java
@@ -0,0 +1,13 @@
+package ml.spmc.invapi;
+
+public class InventoryAPI extends JavaPlugin {
+   @Override
+   public void onEnable() {
+     System.out.println("[InventoryAPI] Loaded!");
+   }
+  
+   @Override
+   public void onDisable() {
+     System.out.println("[InventoryAPI] Goodbye!");
+   }  
+}
